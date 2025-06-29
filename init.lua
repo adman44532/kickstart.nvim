4857d2dbecb40fb13a015e783c699c6d59e834c1
adman44532 <adman44532@gmail.com>
2025-06-29 22:06:17 +1000

    wip: nixd support

--- init.lua
+++ init.lua
@@ -90,7 +90,7 @@ local function is_nixos()
       vim.fn.isdirectory('/nix/var/nix/profiles/system') == 1
 end
 local use_mason = not is_nixos()
-local nix_flake_path = os.getenv("HOME") .. "/.dotfiles/flake.nix"
+local nix_flake_path = vim.fn.expand("~/.dotfiles")
 
 
 
@@ -744,22 +744,29 @@ require('lazy').setup({
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
+        -- Some languagest (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
         nixd = {
-          nixpkgs = {
-            expr = "import <nixpkgs> { }",
-          },
-          options = {
-            nixos = {
-              expr = '(builtins.getFlake "' .. nix_flake_path .. '").nixosConfigurations.hephaestus.options',
-            },
-            home_manager = {
-              expr = '(builtins.getFlake "' .. nix_flake_path .. '").homeConfigurations.hephaestus.options',
+          settings = {
+            nixd = {
+              nixpkgs = {
+                expr = "import <nixpkgs> { }",
+              },
+              formatting = {
+                command = { "alejandra" },
+              },
+              options = {
+                nixos = {
+                  expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.hephaestus.options',
+                },
+                home_manager = {
+                  expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations.hephaestus.options',
+                },
+              },
             },
           },
         },
