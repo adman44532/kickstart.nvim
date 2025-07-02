624d1d483676bf58e97eb2d4409cfc0f1de518b3
adman44532 <adman44532@gmail.com>
2025-07-03 09:22:18 +1000

    add: comment.nvim

--- init.lua
+++ init.lua
@@ -288,7 +288,7 @@ rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
-
+  'numToStr/Comment.nvim',
   {
     'catppuccin/nvim',
     name = 'catppuccin',
